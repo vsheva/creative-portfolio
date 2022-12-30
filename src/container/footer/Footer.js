8c0ac595e52523a3780eed9cc2888dcda426b081
vsheva <valera.sheva@gmail.com>
2022-12-30 02:26:37 -0500

    fix

--- src/container/footer/Footer.js
+++ src/container/footer/Footer.js
@@ -44,7 +44,7 @@ const Footer = () => {
                 </div>
                 <div className="app__footer-card">
                     <img src={images.mobile} alt="phone" />
-                    <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+                    <a href="tel:+1 (646) 946-3888" className="p-text">+1 (646) 946-3888</a>
                 </div>
             </div>
 
